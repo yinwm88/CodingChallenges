9b2732d5b319831495c07f7827532690ea7922b2
wong <yinlen@ciencias.unam.mx>
2024-06-27 17:44:51 -0600

    done

--- KeepItBeautiful.java
+++ KeepItBeautiful.java
@@ -15,25 +15,31 @@ public class KeepItBeautiful {
 
     private static String keepItBeautiful(Scanner scan) {
         StringBuilder result = new StringBuilder();
-        int min=0, x=0;
+        int min=0, x=0, max=0;
+        boolean hayMin = false;
         int q = scan.nextInt()-1;
         int firstQuery = scan.nextInt();
+        max = firstQuery;
         result.append("1");  
         while(q>0){
             q--;
-            //aux = i;
             x = scan.nextInt();
             result.append("1");
-            if(x<firstQuery){
-                min = x;
-                break;
+            if(x > max){
+                max = x;
+            }
+            if(x < max){
+                if(x<=firstQuery){
+                    min = x;
+                    break;
+                }
             }
         }
         while(q>0){
             q--;
             x = scan.nextInt();
             if(min<=x && x<=firstQuery){
-                result.append("1");  
+                result.append("1");   
             }else{
                 result.append("0");  
             }

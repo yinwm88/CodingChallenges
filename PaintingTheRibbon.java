480eb5a4d8b69a8d4e5ed0112fcf6b414ef7c7bc
wong <yinlen@ciencias.unam.mx>
2024-07-02 21:21:12 -0600

    done

--- PaintingTheRibbon.java
+++ PaintingTheRibbon.java
@@ -16,21 +16,15 @@ public class PaintingTheRibbon{
     }
 
     private static String paintingTheRibbon(int n, int m, int k){
-        if(n == 1 || m == 1 || k >= n || (n==m)&&((k+1) == m) || (k==(n-1))){
-            return "NO";
-        }else{
-            if((k >= (n/m)) && ((n%m)==0 && m==2))return "NO";
             int fmin = n/m;
             int fmax = fmin;
-            if((n%m)!=0){   
+            if((n%m)!=0)
                 fmax = fmin + 1;
-            }
             int nMaxf = n%m;
             int nMinf = n - (nMaxf*fmax);
             int min = nMinf + ((nMaxf*fmax)-fmax);
             if(k>=min)return "NO";
             return "YES";
-        }
     }
 
 }
